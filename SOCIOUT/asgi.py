af764ee1c035654c3c036b4f630becf62cb5f77c
gaurihpatil <gauripatil22001@gmail.com>
2022-11-27 19:25:55 +0530

    Account settings

---
+++ SOCIOUT/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SOCIOUT project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOCIOUT.settings')
+
+application = get_asgi_application()
