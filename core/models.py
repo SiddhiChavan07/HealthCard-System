3896709d4692f6afa53b4f9b0771ab07302d30dd
MugdhaJathar <jatharmugdha0205@gmail.com>
2022-11-28 20:02:47 +0530

    final

--- core/models.py
+++ core/models.py
@@ -32,6 +32,12 @@ class LikePost(models.Model):
     post_id = models.CharField(max_length=500)
     username = models.CharField(max_length=100)
 
-    def _str_(self):
+    def __str__(self):
         return self.username
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
 
